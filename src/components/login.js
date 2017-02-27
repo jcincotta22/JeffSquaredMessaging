4afedefe34aa15748ba6bb404653f9907ed86dcc
jcincotta22 <jcincotta22@gmail.com>
2017-02-26 19:25:14 -0500

    login input button

--- src/components/login.js
+++ src/components/login.js
@@ -3,17 +3,44 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  TextInput,
+  TouchableHighlight,
   StyleSheet
 } from 'react-native';
 
 
 export default React.createClass({
+  getInitialState: function() {
+    return {
+      username: ''
+    };
+  },
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={{color: 'white'}}>SendBird JavaScript SDK!!!</Text>
+        <View style={styles.loginContainer}>
+          <TextInput
+            style={styles.input}
+            value={this.state.username}
+            onChangeText={(text) => this.setState({username: text})}
+            placeholder={'Enter User Nickname'}
+            maxLength={12}
+            multiline={false}
+            />
+
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor={'#328FE6'}
+            onPress={this.onPress}
+            >
+            <Text style={styles.label}>LOGIN</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
+  },
+  onPress: function() {
+    console.log(this.state.username);
   }
 });
 
@@ -23,5 +50,41 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'stretch',
     backgroundColor: '#6E5BAA'
+  },
+  loginContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: 250,
+    color: '#555555',
+    padding: 10,
+    height: 50,
+    borderColor: '#32C5E6',
+    borderWidth: 1,
+    borderRadius: 4,
+    alignSelf: 'center',
+    backgroundColor: '#ffffff'
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 50,
+    borderColor: '#328FE6',
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#32c5e6'
+  },
+  label: {
+    width: 230,
+    flex: 1,
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#ffffff'
   }
 });
